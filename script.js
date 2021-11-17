8fdfa731841250f9a8a549af2c7c8aa536faf062
Jesus Cifuentes <22laloc@gmail.com>
2021-11-16 20:36:29 -0600

    first set of questions set up

---
+++ script.js
@@ -0,0 +1,41 @@
+
+let questions = [
+{
+    question: "What Hogwarts house is Harry Potter in?",
+    answers: ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"],
+    correctAnswer: 0
+    
+},
+{
+    question: "What is Harry Potter's position on the Quidditch team?",
+    answers: ["Beater", "Chaser", "Seeker", "Keeper"],
+    correctAnswer: 2
+
+},
+{
+    question: "How many staircases are located in Hogwarts?",
+    answers:["345", "446", "142","240"],
+    correctAnswer: 2
+},
+{
+    question: "What type of Dragon does Harry fight during the Tri-Wizard tournament?",
+    answers:["Norwegian Ridgeback","Chinese Fireball", "Swedish Short-Snout", "Hungarian Horntail"],
+    correctAnswer: 3
+},
+{
+    
+}
+]
+
+let currentQuestion = 0
+const question = questions[currentQuestion];
+
+document.querySelector("#question").textContent = questions[currentQuestion].question;
+document.querySelector("#btn1").textContent = questions[currentQuestion].answers[0]; 
+
+
+//     for (let i = 0; i < questions.length; i++) {
+//     const element = questions[i];
+
+//     questions[i].answers
+// }
